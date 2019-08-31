b84312111a230d5f11f77cfe955b1e9eaf74b91c
RektInator <mlmmlangeveld@gmail.com>
2019-08-31 12:52:54 +0200

    Added directory support

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,38 @@
 #include "logger.hpp"
 #include "ifs.hpp"
 
+void handle_archives(const logger& logger, const std::vector<std::string>& archives)
+{
+	for (auto i = 0u; i < archives.size(); ++i)
+	{
+		std::filesystem::path archive(archives[i]);
+		logger.info("Handling archive %s...\n", archive.filename().string().data());
+
+		const ifs ifs(archives[i]);
+		const auto files = ifs.get_file_list();
+
+		if (files.empty())
+		{
+			logger.warn("No files to extract!\n");
+			continue;
+		}
+
+		logger.info("Extracting %d files...\n", files.size());
+
+		for (const auto& file : files)
+		{
+			if (ifs.extract_file(file, file))
+			{
+				logger.info("Extracted %s\n", file.data());
+			}
+			else
+			{
+				logger.error("Failed to extract %s\n", file.data());
+			}
+		}
+	}
+}
+
 int main(const int argc, char* argv[])
 {
 	const logger logger;
@@ -11,34 +43,26 @@ int main(const int argc, char* argv[])
 	{
 		logger.header("IFS Tool by momo5502");
 
-		for (auto i = 1; i < argc; ++i)
+		std::vector<std::string> archives;
+		if (argc == 2 && std::filesystem::is_directory(argv[1]))
 		{
-			std::filesystem::path archive(argv[i]);
-			logger.info("Handling archive %s...\n", archive.filename().string().data());
-
-			const ifs ifs(argv[i]);
-			const auto files = ifs.get_file_list();
-
-			if(files.empty())
+			for (auto& itr : std::filesystem::directory_iterator(argv[1]))
 			{
-				logger.warn("No files to extract!\n");
-				continue;
-			}
-
-			logger.info("Extracting %d files...\n", files.size());
-
-			for (const auto& file : files)
-			{
-				if (ifs.extract_file(file, file))
+				if (itr.is_regular_file() && itr.path().extension() == ".ifs")
 				{
-					logger.info("Extracted %s\n", file.data());
-				}
-				else
-				{
-					logger.error("Failed to extract %s\n", file.data());
+					archives.push_back(itr.path().string());
 				}
 			}
 		}
+		else
+		{
+			for (auto i = 1; i < argc; i++)
+			{
+				archives.push_back(argv[1]);
+			}
+		}
+
+		handle_archives(logger, archives);
 	}
 	catch (std::exception& e)
 	{
